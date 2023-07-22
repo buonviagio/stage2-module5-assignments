b6725e50aa0b671487f77913963ae5e50ca4258a
Shapovalov Dmytro <buonviagio@gmail.com>
2023-07-22 16:13:36 +0200

    My third commit Stage#2 Module#4 Task#5

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -16,24 +16,72 @@ import lombok.Setter;
 @Setter
 public class LocalProcessor {
     private String processorName;
-    private Long period = 10_000_000_000_000L;
+    private long period = 10_000_000_000_000L;
     //protected String processorVersion;
     private String processorVersion;
-    private Integer valueofCheap;
-    private Scanner informationscanner;
+    private int valueOfCheap;
+    private Scanner informationScanner;
     private List<String> stringArrayList;// = new LinkedList<>();
 
-    public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
-                          Scanner informationscanner, List<String> stringArrayList) {
+    public LocalProcessor(String processorName, long period, String processorVersion, int valueOfCheap,
+                          Scanner informationScanner, List<String> stringArrayList) {
         this.processorName = processorName;
         this.period = period;
         this.processorVersion = processorVersion;
-        this.valueofCheap = valueOfCheap;
-        this.informationscanner = informationscanner;
+        this.valueOfCheap = valueOfCheap;
+        this.informationScanner = informationScanner;
         this.stringArrayList = stringArrayList;
     }
 
     public LocalProcessor() {
+    }
+
+    public String getProcessorName() {
+        return processorName;
+    }
+
+    public void setProcessorName(String processorName) {
+        this.processorName = processorName;
+    }
+
+    public long getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(long period) {
+        this.period = period;
+    }
+
+    public String getProcessorVersion() {
+        return processorVersion;
+    }
+
+    public void setProcessorVersion(String processorVersion) {
+        this.processorVersion = processorVersion;
+    }
+
+    public int getValueOfCheap() {
+        return valueOfCheap;
+    }
+
+    public void setValueOfCheap(int valueofCheap) {
+        this.valueOfCheap = valueofCheap;
+    }
+
+    public Scanner getInformationScanner() {
+        return informationScanner;
+    }
+
+    public void setInformationScanner(Scanner informationscanner) {
+        this.informationScanner = informationscanner;
+    }
+
+    public List<String> getStringArrayList() {
+        return stringArrayList;
+    }
+
+    public void setStringArrayList(List<String> stringArrayList) {
+        this.stringArrayList = stringArrayList;
     }
 
     @ListIteratorAnnotation
@@ -58,17 +106,16 @@ public class LocalProcessor {
     }
 
     @ReadFullProcessorNameAnnotation
-    //readfullprocessorname
     public void readFullProcessorName(File file) throws FileNotFoundException {
-            informationscanner = new Scanner(file);
+            informationScanner = new Scanner(file);
             StringBuilder stringBuilder = new StringBuilder();
             try {
-                while (informationscanner.hasNext()) {
+                while (informationScanner.hasNext()) {
                     //ProcessorVersion += informationscanner.nextLine();
-                    stringBuilder.append(informationscanner.nextLine());
+                    stringBuilder.append(informationScanner.nextLine());
                 }
             } catch (IllegalStateException e){
-                informationscanner.close();
+                informationScanner.close();
             }
         processorVersion = stringBuilder.toString();
     }
