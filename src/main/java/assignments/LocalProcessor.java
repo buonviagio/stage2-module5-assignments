ae0cc5e8cedb06c3691f08da06619739c7fbd0b8
Shapovalov Dmytro <buonviagio@gmail.com>
2023-07-22 17:07:38 +0200

    My fifth commit Stage#2 Module#4 Task#5

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -2,15 +2,15 @@ package assignments;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 import assignments.annotations.FullNameProcessorGeneratorAnnotation;
 import assignments.annotations.ListIteratorAnnotation;
 import assignments.annotations.ReadFullProcessorNameAnnotation;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.swing.text.html.Option;
 
 @Getter
 @Setter
@@ -89,7 +89,9 @@ public class LocalProcessor {
         // stringArrayList = new LinkedList<>(stringList);
         stringArrayList = stringList;
         for (int i = 0; i < period; i++) {
-            System.out.println(stringArrayList.get(i).hashCode());
+            if (stringArrayList.get(i) != null) {
+                System.out.println(stringArrayList.get(i).hashCode());
+            }
         }
     }
 
@@ -97,8 +99,9 @@ public class LocalProcessor {
     public String fullNameProcessorGenerator(List<String> stringList) {
         StringBuilder stringBuilder = new StringBuilder();
         //for (int i = 0; i < stringArrayList.size(); i++) {
-        for (String list : stringList){
+        for (String list : stringList) {
             stringBuilder.append(list).append(" ");
+            //stringBuilder.append(" ");
             //processorName+=stringList.get(i)+' ';
         }
         //return processorName;
@@ -107,23 +110,23 @@ public class LocalProcessor {
 
     @ReadFullProcessorNameAnnotation
     public void readFullProcessorName(File file) throws FileNotFoundException {
-            informationScanner = new Scanner(file);
-            StringBuilder stringBuilder = new StringBuilder();
-            try {
-                while (informationScanner.hasNext()) {
-                    //ProcessorVersion += informationscanner.nextLine();
-                    stringBuilder.append(informationScanner.nextLine());
-                }
-
-            } catch (IllegalStateException e){
-                System.out.println(e);
-            } finally {
-                 try {
-                     informationScanner.close();
-                 } catch (IllegalStateException e){
-                     System.out.println(e);
-                 }
+        informationScanner = new Scanner(file);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            while (informationScanner.hasNext()) {
+                //ProcessorVersion += informationscanner.nextLine();
+                stringBuilder.append(informationScanner.nextLine());
             }
+
+        } catch (IllegalStateException e) {
+            System.out.println(e);
+        } finally {
+            try {
+                informationScanner.close();
+            } catch (IllegalStateException e) {
+                System.out.println(e);
+            }
+        }
         processorVersion = stringBuilder.toString();
     }
 }
